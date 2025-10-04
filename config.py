f34180977028591a0c01a094498528231e920143
rangga8779 <naufalrangga753@gmail.com>
2025-10-04 07:19:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 API_ID :int = int(os.environ.get("API_ID", None))
-API_HASH :str = os.environ.get("API_HASH", "")
-BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "")
-UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "mr_sukkun")
+API_HASH :str = os.environ.get("API_HASH", "00c8e35df83fefbe7f9e38b8c4f3b0e9")
+BOT_TOKEN :str= os.environ.get("BOT_TOKEN", "8307340818:AAGkBAqEbjwtt6nD-WoZ-Qfz1sGrUJU25zg")
+UPDATE_CHNL :str = os.environ.get("UPDATE_CHNL", "Gilbertallpthare")
 SUPPORT_GRP :str = os.environ.get("SUPPORT_GRP", "the_support_chat")
 START_IMG :str = os.environ.get(
     "START_IMG", "https://graph.org/file/d4412c7b411ca8da9e177.jpg"
@@ -12,5 +12,5 @@ MONGO_DB_URI :str = os.environ.get(
     "MONGO_DB_URI",
     "",
 )
-OWNER_ID :int= os.environ.get("OWNER_ID", 6728038801)
+OWNER_ID :int= os.environ.get("OWNER_ID", 7056257981)
 
